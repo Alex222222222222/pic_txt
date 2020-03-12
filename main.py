5d02a9444f397c0e5a99b3f007c606c7757735c0
hhhh hhhh <hhhhhhhh@hhhhdeMacBook-Air.local>
2020-03-12 13:26:10 +0800

    2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import PIL
 import math
+import os
 from PIL import Image
 #用于将图片缩小至合适的大小
 
@@ -10,11 +11,22 @@ def reduce(path):
     width_=512
     k=width_/width
     height_=math.floor(height*k)
-    print(str(width_)+','+str(height_))
     pic.thumbnail((width_,height_))
     pic.save('done.png','png')
     return()
 
+def grew(path):
+    pic=Image.open(path,'r')
+    pic=pic.convert('L')
+    pic.save('grew.png','png')
+    pic.close
+    os.remove(path)
+
+def dic(path):
+    file=open(path):
+    
+
 if __name__=='__main__':
     path='test.png'
-    print(reduce(path))
+    reduce(path)
+    grew('done.png')
